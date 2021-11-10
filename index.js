210ab4c445f035187c4573a59db05257b844457a
jvredmoore <jvredmoore@gmail.com>
2021-11-10 15:49:52 +0000

    worked on code challenge

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+function stripUpperCase(string){
+    let stripped = ""
+    for(let i of string){
+        if(i === i.toLowerCase()){
+            stripped += i;
+        }
+    }return stripped;
+}
+
+console.log(stripUpperCase("Hello!"))
